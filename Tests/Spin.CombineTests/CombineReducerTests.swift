43b4937360eb4c1cfc7fd618c820c9c41cd87481
Thibault Wittemberg <thibault.wittemberg@gmail.com>
2020-02-09 16:33:45 -0500

    combinereducer: fix a flaky test

--- Tests/Spin.CombineTests/CombineReducerTests.swift
+++ Tests/Spin.CombineTests/CombineReducerTests.swift
@@ -12,34 +12,39 @@ import XCTest
 
 final class CombineReducerTests: XCTestCase {
 
+    private var disposeBag = [AnyCancellable]()
+
     func test_reducer_is_performed_on_default_executer_when_no_executer_is_specified() throws {
         // Given: an event stream switching on a specified Executer after being executed
         // and a Reducer applied after this event stream
+        let exp = expectation(description: "default executer for reducer")
         var reduceIsCalled = false
         let expectedExecuterName = "com.apple.main-thread"
         var receivedExecuterName = ""
 
         let inputStreamSchedulerQueueLabel = "INPUT_STREAM_QUEUE_\(UUID().uuidString)"
         let inputStreamScheduler = DispatchQueue(label: inputStreamSchedulerQueueLabel,
-                                                 qos: .userInitiated,
-                                                 attributes: .concurrent)
+                                                 qos: .userInitiated)
 
         let eventStream = Just("").receive(on: inputStreamScheduler).eraseToAnyPublisher()
 
         let reducerFunction = { (state: String, action: String) -> String in
             reduceIsCalled = true
             receivedExecuterName = DispatchQueue.currentLabel
+            exp.fulfill()
             return ""
         }
 
         // When: reducing without specifying an Executer for the reduce operation
         let sut = CombineReducer(reducer: reducerFunction)
 
-        let recorder = sut.reducerOnExecuter("initialState", eventStream)
+        sut.reducerOnExecuter("initialState", eventStream)
             .output(in: (0...1))
-            .record()
+            .eraseToAnyPublisher()
+            .spin()
+            .disposed(by: &self.disposeBag)
 
-        _ = try wait(for: recorder.completion, timeout: 5)
+        waitForExpectations(timeout: 5)
 
         // Then: the reduce is performed
         // Then: the reduce is performed on the default executer, ie the main queue for CombineReducer
@@ -49,34 +54,36 @@ final class CombineReducerTests: XCTestCase {
 
     func test_reducer_is_performed_on_dedicated_executer_when_executer_is_specified() throws {
         // Given: a effect switching on a specified Executer after being executed
+        let exp = expectation(description: "default executer for reducer")
         var reduceIsCalled = false
         let expectedExecuterName = "REDUCER_QUEUE_\(UUID().uuidString)"
         var receivedExecuterName = ""
 
         let inputStreamSchedulerQueueLabel = "INPUT_STREAM_QUEUE_\(UUID().uuidString)"
         let inputStreamScheduler = DispatchQueue(label: inputStreamSchedulerQueueLabel,
-                                                 qos: .userInitiated,
-                                                 attributes: .concurrent)
+                                                 qos: .userInitiated)
         let reducerScheduler = DispatchQueue(label: expectedExecuterName,
-                                             qos: .userInitiated,
-                                             attributes: .concurrent).eraseToAnyScheduler()
+                                             qos: .userInitiated).eraseToAnyScheduler()
 
         let eventStream = Just("").receive(on: inputStreamScheduler).eraseToAnyPublisher()
 
         let reducerFunction = { (state: String, action: String) -> String in
             reduceIsCalled = true
             receivedExecuterName = DispatchQueue.currentLabel
+            exp.fulfill()
             return ""
         }
 
         // When: reducing with specifying an Executer for the reduce operation
         let sut = CombineReducer(reducer: reducerFunction, on: reducerScheduler)
 
-        let recorder = sut.reducerOnExecuter("initialState", eventStream)
+        sut.reducerOnExecuter("initialState", eventStream)
             .output(in: (0...1))
-            .record()
+            .eraseToAnyPublisher()
+            .spin()
+            .disposed(by: &self.disposeBag)
 
-        _ = try wait(for: recorder.elements, timeout: 5)
+        waitForExpectations(timeout: 5)
 
         // Then: the reduce is performed
         // Then: the reduce is performed on the current executer, ie the one set by the effect
